c9078282232d80c62fd99ca83afc4ef98294b81c
matheusmenezs <matheushm1798@gmail.com>
2022-03-26 16:08:18 -0300

    refactor: refactor the structure of the server and app

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,34 +1,10 @@
-import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import routes from './routes';
-import AppError from '@shared/errors/AppError';
-import { connectDB } from '@shared/typeorm';
-import '@shared/typeorm';
+import { app } from './app';
 
-const app = express();
-const port = 3000;
+const port = 3333;
 
-app.use(cors());
-app.use(express.json());
+const server = app.listen(port, () => console.log(`Server started on port ${port}`));
 
-app.use(routes);
-
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response
-        .status(error.statusCode)
-        .json({ status: 'error', message: error.message });
-    }
-    return response
-      .status(500)
-      .json({ status: 'error', message: 'Internal server error' });
-  },
-);
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+process.on('SIGINT', () => {
+  server.close();
+  console.log(`Server connection closed`);
 });
-
-connectDB();
