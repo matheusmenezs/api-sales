6a73d36c1259da13cc5384e39813fd269d9fc402
matheusmenezs <matheushm1798@gmail.com>
2022-03-26 16:21:00 -0300

    feat: import the routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,10 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
+import productsRouter from '@modules/products/routes/ProductsRoutes';
+import usersRouter from '@modules/users/routes/UsersRoutes';
 
 const routes = Router();
 
-routes.get('/', async (request: Request, response: Response) => {
-  response.status(200).json({ message: 'Hello World!' });
-});
+routes.use('/products', productsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
