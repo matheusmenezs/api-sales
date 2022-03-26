b5d74644dcd930f9f0d265407a747a43c2ba1e51
matheusmenezs <matheushm1798@gmail.com>
2022-03-26 15:57:02 -0300

    refactor: change the method to initialize the database connection

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,14 +1,8 @@
-import { dataSource } from 'src/shared/typeorm/ormconfig';
+import { dataSource } from './datasource';
 
-export const connectDB = async () => {
-  const connection = await dataSource.connect();
-  console.log(
-    `Connection established with database ${connection.options.database}`,
-  );
+dataSource.initialize();
+console.log(`Connection established with database ${dataSource.options.database}`);
 
-  process.on('SIGINT', () => {
-    connection
-      .close()
-      .then(() => console.log(`Database connection was closed`));
-  });
-};
+process.on('SIGINT', () => {
+  dataSource.destroy().then(() => console.log(`Database connection was closed`));
+});
