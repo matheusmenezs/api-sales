336e067bc3fce786dbf256207d0068411fd81e33
matheusmenezs <matheushm1798@gmail.com>
2022-03-26 16:17:41 -0300

    feat: create user repository

---
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,34 @@
+import { dataSource } from '@shared/typeorm/datasource';
+import User from '../entities/User';
+
+export const UsersRepository = dataSource.getRepository(User).extend({
+  async findByName(name: string): Promise<User | null> {
+    const user = await this.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return user;
+  },
+
+  async findById(id: string): Promise<User | null> {
+    const user = await this.findOne({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  },
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.findOne({
+      where: {
+        email,
+      },
+    });
+
+    return user;
+  },
+});
