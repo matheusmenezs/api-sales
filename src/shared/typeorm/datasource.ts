d97349876a5d4a76aae8796d5d8ef57a016e6c86
matheusmenezs <matheushm1798@gmail.com>
2022-03-26 16:30:53 -0300

    feat: create typeorm datasource

---
+++ src/shared/typeorm/datasource.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5433,
+  username: 'postgres',
+  password: 'docker',
+  database: 'apisales',
+  synchronize: true,
+  entities: ['./src/modules/**/typeorm/entities/*.ts'],
+  migrations: ['./src/shared/typeorm/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/shared/typeorm/migrations',
+  },
+});
