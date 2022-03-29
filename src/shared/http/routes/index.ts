8810045f95872f1e829d11ff33fdce50dfc2cba5
matheusmenezs <matheushm1798@gmail.com>
2022-03-29 08:44:46 -0300

    feat: import session routes in app

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import productsRouter from '@modules/products/routes/ProductsRoutes';
 import usersRouter from '@modules/users/routes/UsersRoutes';
+import sessionsRouter from '@modules/users/routes/SessionsRoutes';
 
 const routes = Router();
 
+routes.use('/sessions', sessionsRouter);
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 
