1518947bc7601a0d67139fcce630ebfdb072e0be
matheusmenezs <matheushm1798@gmail.com>
2022-04-03 10:52:07 -0300

    feat: add password route in app

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import productsRouter from '@modules/products/routes/ProductsRoutes';
 import usersRouter from '@modules/users/routes/UsersRoutes';
 import sessionsRouter from '@modules/users/routes/SessionsRoutes';
+import passwordRouter from '@modules/users/routes/PasswordRoutes';
 
 const routes = Router();
 
 routes.use('/sessions', sessionsRouter);
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
