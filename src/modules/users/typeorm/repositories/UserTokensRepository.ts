423d9077b4b09b871780cf3a7946279aab4ab5c6
matheusmenezs <matheushm1798@gmail.com>
2022-04-03 10:44:50 -0300

    feat: create custom token repository

---
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,23 @@
+import { dataSource } from '@shared/typeorm/datasource';
+import UserToken from '../entities/UserToken';
+
+export const UserTokensRepository = dataSource.getRepository(UserToken).extend({
+  async findByToken(token: string): Promise<UserToken | null> {
+    const userToken = await this.findOne({
+      where: {
+        token,
+      },
+    });
+
+    return userToken;
+  },
+  async generate(user_id: string): Promise<UserToken | null> {
+    const userToken = await this.create({
+      user_id,
+    });
+
+    await this.save(userToken);
+
+    return userToken;
+  },
+});
