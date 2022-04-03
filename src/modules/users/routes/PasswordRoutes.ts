98d4f85bbb238a50865c3883d2a5aa8b7a195d52
matheusmenezs <matheushm1798@gmail.com>
2022-04-03 12:30:58 -0300

    feat: create password reset route

--- src/modules/users/routes/PasswordRoutes.ts
+++ src/modules/users/routes/PasswordRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import ForgotPasswordController from '../controllers/ForgotPasswordController';
-import { emailValidator } from './UserValidator';
+import ResetPasswordController from '../controllers/ResetPasswordController';
+import { emailValidator, resetValidator } from './PasswordValidator';
 
 const passwordRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
 
 passwordRouter.post('/forgot', emailValidator, forgotPasswordController.generate);
+passwordRouter.post('/reset', resetValidator, resetPasswordController.create);
 
 export default passwordRouter;
