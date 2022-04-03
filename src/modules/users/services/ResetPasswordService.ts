8e47f035d4d109b9f412356447f36c8fe4cec121
matheusmenezs <matheushm1798@gmail.com>
2022-04-03 10:47:42 -0300

    feat: create reset password service

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,37 @@
+import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
+import { addHours, isAfter } from 'date-fns';
+import { UsersRepository } from '../typeorm/repositories/UsersRepository';
+import { UserTokensRepository } from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordService {
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const userToken = await UserTokensRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('Token does not exists.');
+    }
+
+    const user = await UsersRepository.findById(userToken.user_id);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expired');
+    }
+
+    user.password = await hash(password, 8);
+  }
+}
+
+export default ResetPasswordService;
