7599d9b30d9fc2eb3e54d342cfed6591e331623d
matheusmenezs <matheushm1798@gmail.com>
2022-07-26 12:25:03 -0300

    refactor: fix typeorm migrations

--- src/shared/typeorm/datasource.ts
+++ src/shared/typeorm/datasource.ts
@@ -1,4 +1,10 @@
+import Product from '@modules/products/typeorm/entities/Product';
+import User from '@modules/users/typeorm/entities/User';
+import UserToken from '@modules/users/typeorm/entities/UserToken';
 import { DataSource } from 'typeorm';
+import { CreateProducts1607437608841 } from './migrations/1647802471827-CreateProducts';
+import { CreateUsers1648207407461 } from './migrations/1648317183372-CreateUsers';
+import { CreateUsersTokens1648821815049 } from './migrations/1648821815049-CreateUsersTokens';
 
 export const dataSource = new DataSource({
   type: 'postgres',
@@ -7,9 +13,10 @@ export const dataSource = new DataSource({
   username: 'postgres',
   password: 'postgres',
   database: 'dbsales',
-  migrations: ['./src/shared/typeorm/migrations/*.ts'],
-  entities: ['./src/modules/**/typeorm/entities/*.ts'],
-  cli: {
-    migrationsDir: './src/shared/typeorm/migrations',
-  },
+  entities: [User, UserToken, Product],
+  migrations: [
+    CreateUsers1648207407461,
+    CreateUsersTokens1648821815049,
+    CreateProducts1607437608841,
+  ],
 });
