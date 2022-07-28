03c9857f8a93fbcf252b338a99b52c6c526aea59
matheusmenezs <matheushm1798@gmail.com>
2022-07-28 17:44:32 -0300

    feat: create order services

---
+++ src/modules/orders/services/ShowOrderService.ts
@@ -0,0 +1,21 @@
+import AppError from '@shared/errors/AppError';
+import Order from '../typeorm/entities/Order';
+import { OrdersRepository } from '../typeorm/repositories/OrdersRepository';
+
+interface IRequest {
+  id: string;
+}
+
+class ShowOrderService {
+  public async execute({ id }: IRequest): Promise<Order> {
+    const order = await OrdersRepository.findById(id);
+
+    if (!order) {
+      throw new AppError('Order not found.');
+    }
+
+    return order;
+  }
+}
+
+export default ShowOrderService;
