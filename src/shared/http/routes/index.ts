b2c9ad6ee01da21f0efcba5905b4e39d1d5a2b10
matheusmenezs <matheushm1798@gmail.com>
2022-07-28 17:52:29 -0300

    feat: import new routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,9 @@ import productsRouter from '@modules/products/routes/ProductsRoutes';
 import usersRouter from '@modules/users/routes/UsersRoutes';
 import sessionsRouter from '@modules/users/routes/SessionsRoutes';
 import passwordRouter from '@modules/users/routes/PasswordRoutes';
+import profileRouter from '@modules/users/routes/ProfileRoutes';
+import customersRouter from '@modules/customers/routes/customers.routes';
+import ordersRouter from '@modules/orders/routes/orders.routes';
 
 const routes = Router();
 
@@ -10,5 +13,8 @@ routes.use('/sessions', sessionsRouter);
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
+routes.use('/customers', customersRouter);
+routes.use('/orders', ordersRouter);
 
 export default routes;
