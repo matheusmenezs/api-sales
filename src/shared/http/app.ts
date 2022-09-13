3604e80100230f554c4c44a9cafb161ac9579651
matheusmenezs <matheushm1798@gmail.com>
2022-09-13 10:31:31 -0300

    feat: use rate limiter in app

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -8,24 +8,30 @@ import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
 import uploadConfig from '@config/upload';
+import rateLimiter from './middlewares/rateLimiter';
 
 export const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(rateLimiter);
+
 app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
 
 app.use(errors());
 
-app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response
+        .status(error.statusCode)
+        .json({ status: 'error', message: error.message });
+    }
     return response
-      .status(error.statusCode)
-      .json({ status: 'error', message: error.message });
-  }
-  return response
-    .status(500)
-    .json({ message: 'Internal server error', error: error });
-});
+      .status(500)
+      .json({ message: 'Internal server error', error: error });
+  },
+);
