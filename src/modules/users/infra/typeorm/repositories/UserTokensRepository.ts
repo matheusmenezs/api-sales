844c579fd7efde8b68b3410002ef086a4e43db8d
matheusmenezs <matheushm1798@gmail.com>
2022-09-24 11:44:51 -0300

    refactor: refactor user repository

--- src/modules/users/infra/typeorm/repositories/UserTokensRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UserTokensRepository.ts
@@ -1,23 +1,32 @@
+import { IUserTokenRepository } from '@modules/users/domain/repositories/IUserTokenRepository';
 import { dataSource } from '@shared/infra/typeorm/datasource';
+import { Repository } from 'typeorm';
 import UserToken from '../entities/UserToken';
 
-export const UserTokensRepository = dataSource.getRepository(UserToken).extend({
-  async findByToken(token: string): Promise<UserToken | null> {
-    const userToken = await this.findOne({
+export class UserTokensRepository implements IUserTokenRepository {
+  private ormRepository: Repository<UserToken>;
+
+  constructor() {
+    this.ormRepository = dataSource.getRepository(UserToken);
+  }
+
+  public async findByToken(token: string): Promise<UserToken | null> {
+    const userToken = await this.ormRepository.findOne({
       where: {
         token,
       },
     });
 
     return userToken;
-  },
-  async generate(user_id: string): Promise<UserToken> {
-    const userToken = await this.create({
+  }
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = this.ormRepository.create({
       user_id,
     });
 
-    await this.save(userToken);
+    await this.ormRepository.save(userToken);
 
     return userToken;
-  },
-});
+  }
+}
--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -1,34 +1,65 @@
+import { Repository } from 'typeorm';
+import { IUserRepository } from '@modules/users/domain/repositories/IUserRepository';
 import { dataSource } from '@shared/infra/typeorm/datasource';
 import User from '../entities/User';
+import { IUser } from '@modules/users/domain/model/IUser';
+import { ICreateUser } from '@modules/users/domain/model/ICreateUser';
 
-export const UsersRepository = dataSource.getRepository(User).extend({
-  async findByName(name: string): Promise<User | null> {
-    const user = await this.findOne({
+export class UsersRepository implements IUserRepository {
+  private ormRepository: Repository<IUser>;
+
+  constructor() {
+    this.ormRepository = dataSource.getRepository(User);
+  }
+
+  public async findByName(name: string): Promise<IUser | null> {
+    const user = await this.ormRepository.findOne({
       where: {
         name,
       },
     });
 
     return user;
-  },
+  }
 
-  async findById(id: string): Promise<User | null> {
-    const user = await this.findOne({
+  public async findById(id: string): Promise<IUser | null> {
+    const user = await this.ormRepository.findOne({
       where: {
         id,
       },
     });
 
     return user;
-  },
+  }
 
-  async findByEmail(email: string): Promise<User | null> {
-    const user = await this.findOne({
+  public async findByEmail(email: string): Promise<IUser | null> {
+    const user = await this.ormRepository.findOne({
       where: {
         email,
       },
     });
+    return user;
+  }
+
+  public async create({ name, email, password }: ICreateUser): Promise<IUser> {
+    const user = this.ormRepository.create({ name, email, password });
+
+    await this.ormRepository.save(user);
 
     return user;
-  },
-});
+  }
+
+  public async save(user: IUser): Promise<IUser> {
+    await this.ormRepository.save(user);
+
+    return user;
+  }
+
+  public async remove(user: IUser): Promise<void> {
+    await this.ormRepository.remove(user);
+  }
+  public async findAll(): Promise<IUser[]> {
+    const users = await this.ormRepository.find();
+    return users;
+  }
+}
