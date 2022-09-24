f8992143da16f5b1e46aef85685878696ecc92a2
matheusmenezs <matheushm1798@gmail.com>
2022-09-24 11:42:21 -0300

    refactor: register user singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,22 @@ import { container } from 'tsyringe';
 
 import { ICustomersRepository } from '@modules/customers/domain/repositories/ICustomersRepository';
 import CustomersRepository from '@modules/customers/infra/typeorm/repositories/CustomersRepository';
+import { IUserRepository } from '@modules/users/domain/repositories/IUserRepository';
+import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import { IUserTokenRepository } from '@modules/users/domain/repositories/IUserTokenRepository';
+import { UserTokensRepository } from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 container.registerSingleton<ICustomersRepository>(
   'CustomersRepository',
   CustomersRepository,
+);
+
+container.registerSingleton<IUserRepository>(
+  'UsersRepository',
+  UsersRepository,
+);
+
+container.registerSingleton<IUserTokenRepository>(
+  'UsersTokenRepository',
+  UserTokensRepository,
 );
