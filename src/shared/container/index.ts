4989f6a8f50660c5d3870ee4ec6b34cc08bb8e21
matheusmenezs <matheushm1798@gmail.com>
2022-09-25 19:53:27 -0300

    feat: register singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,10 +6,24 @@ import { IUserRepository } from '@modules/users/domain/repositories/IUserReposit
 import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import { IUserTokenRepository } from '@modules/users/domain/repositories/IUserTokenRepository';
 import { UserTokensRepository } from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+import { IProductsRepository } from '@modules/products/domain/repositories/IProductsRepository';
+import { ProductsRepository } from '@modules/products/infra/typeorm/repositories/ProductsRepository';
+import { IOrdersRepository } from '@modules/orders/domain/repositories/IOrdersRepository';
+import { OrdersRepository } from '@modules/orders/infra/typeorm/repositories/OrdersRepository';
 
 container.registerSingleton<ICustomersRepository>(
   'CustomersRepository',
   CustomersRepository,
+);
+
+container.registerSingleton<IProductsRepository>(
+  'ProductsRepository',
+  ProductsRepository,
+);
+
+container.registerSingleton<IOrdersRepository>(
+  'OrdersRepository',
+  OrdersRepository,
 );
 
 container.registerSingleton<IUserRepository>(
